8d8a24759857d776fe5549d258e2af25052da551
samiha <zaheer.gauhar@aalto.fi>
2022-02-24 15:58:32 +0200

    calculation for number of posts in given month

--- module/Statistics/src/Calculator/NoopCalculator.php
+++ module/Statistics/src/Calculator/NoopCalculator.php
@@ -9,12 +9,28 @@ use Statistics\Dto\StatisticsTo;
 
 class NoopCalculator extends AbstractCalculator
 {
+
+    protected const UNITS = 'posts';
+
+    /**
+     * @var array
+     */
+    private $userCount = [];
+
+    /**
+     * @var int
+    */
+    private $postCount = 0;
+
     /**
      * @inheritDoc
      */
     protected function doAccumulate(SocialPostTo $postTo): void
     {
-        // Noops!
+        $this->postCount++;
+        $key = $postTo->getAuthorName();
+        //array to store users and their number of posts
+        $this->userCount[$key] = ($this->userCount[$key] ?? 0) + 1;
     }
 
     /**
@@ -22,6 +38,26 @@ class NoopCalculator extends AbstractCalculator
      */
     protected function doCalculate(): StatisticsTo
     {
-        return new StatisticsTo();
+        $stats = new StatisticsTo();
+        //caculating average number of posts per user in given month parameter
+        $value = $this->postCount > 0
+        ? $this->postCount / count($this->userCount)
+        : 0;
+        $stats->setValue(round($value,2));
+
+        /* 
+            //this code will be use to show list of users and number of posts by them in the given month
+            foreach ($this->userCount as $splitPeriod => $posts) {
+                $child = (new StatisticsTo())
+                    ->setName($this->parameters->getStatName())
+                    ->setSplitPeriod($splitPeriod)
+                    ->setValue($posts)
+                    ->setUnits(self::UNITS);
+
+                $stats->addChild($child);
+            }
+        */
+
+        return $stats;
     }
 }
